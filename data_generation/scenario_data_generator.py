6b5aaac0aa24951afea3d31de255b41162478f6e
pieter98 <pieter.pangat@outlook.com>
2021-04-09 22:15:29 +0200

    minor changes to the basic data generator

--- data_generation/scenario_data_generator.py
+++ data_generation/scenario_data_generator.py
@@ -1,4 +1,5 @@
 import os, sys
+import igraph
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import copy
 import random
@@ -19,43 +20,43 @@ class ScenarioDataGenerator:
             spinner.write("LOADING BLOCKS:")
 
             spinner.write("  > Loading base")
-            self.base_tree = self.load_base_tree()
+            self.base_tree = self.__load_base_tree__()
 
             spinner.write("  > Loading DC motors")
-            self.dc_motors = self.load_dc_motors()
+            self.dc_motors = self.__load_dc_motors__()
 
             spinner.write("  > Loading Directions")
-            self.directions = self.load_directions()
+            self.directions = self.__load_directions__()
 
             spinner.write("  > Loading While Loops")
-            self.while_loops = self.load_while_loops()
+            self.while_loops = self.__load_while_loops__()
             
             spinner.write("  > Loading For Loops")
-            self.for_loops = self.load_for_loops()
+            self.for_loops = self.__load_for_loops__()
 
             spinner.write(" > Loading variable increments")
-            self.variable_increment = self.load_variable_increment()
+            self.variable_increment = self.__load_variable_increment__()
 
             spinner.ok("✔")
 
     def generate_code_test(self):
         new_tree = copy.deepcopy(self.base_tree)
         setup_loop = new_tree.getroot()[1]
-        root = self.add_loop(setup_loop[1],False)
-        root = self.add_loop(root, True)
-        root = self.add_loop(root, True)
-        self.add_dc_motors_in_loop(root)
+        root = self.__add_loop__(setup_loop[1],False)
+        root = self.__add_loop__(root, True)
+        # root = self.add_loop(root, True)
+        self.__add_dc_motors_in_loop__(root)
         with open('generator-test.xml', 'wb') as f:
             new_tree.write(f)
         
         return new_tree.getroot()
 
     #=========== LOAD BLOCKS FROM XML FILES ===========#
-    def load_base_tree(self):
+    def __load_base_tree__(self):
         base_tree = ET.parse("./data_generation/Blocks/base.xml")
         return base_tree
 
-    def load_dc_motors(self):        
+    def __load_dc_motors__(self):        
         dc_motors = []
         dc_motors_tree = ET.parse("data_generation/Blocks_scenario/driving robot dc motors.xml")
         for child in list(dc_motors_tree.getroot()):
@@ -63,7 +64,7 @@ class ScenarioDataGenerator:
                 dc_motors.append(child)
         return dc_motors
     
-    def load_directions(self):
+    def __load_directions__(self):
         directions = []
         drive_straight = ET.parse("data_generation/Blocks_scenario/driving robot drive straight.xml")
         turn_left = ET.parse("data_generation/Blocks_scenario/driving robot turn left.xml")
@@ -73,7 +74,7 @@ class ScenarioDataGenerator:
                 directions.append(child)
         return directions
 
-    def load_while_loops(self):
+    def __load_while_loops__(self):
         while_loops = []
         while_loops_tree = ET.parse("data_generation/Blocks_scenario/driving robot while loops.xml")
         for child in list(while_loops_tree.getroot()):
@@ -81,7 +82,7 @@ class ScenarioDataGenerator:
                 while_loops.append(child)
         return while_loops
     
-    def load_for_loops(self):
+    def __load_for_loops__(self):
         for_loops = []
         for_loops_tree = ET.parse("data_generation/Blocks_scenario/driving robot for loops.xml")
         for child in list(for_loops_tree.getroot()):
@@ -89,7 +90,7 @@ class ScenarioDataGenerator:
                 for_loops.append(child)
         return for_loops
 
-    def load_variable_increment(self):
+    def __load_variable_increment__(self):
         variable_increment = []
         for child in list(ET.parse("data_generation/Blocks_scenario/driving robot variable increment.xml").getroot()):
             if '}block' in child.tag:
@@ -98,22 +99,22 @@ class ScenarioDataGenerator:
 
     #=========== ADD BLOCKS ===========#
 
-    def add_loop(self, root, add_next):
+    def __add_loop__(self, root, add_next):
         if random.randint(0,100) < 50:
-            loop = self.add_while_loop(root, add_next)
+            loop = self.__add_while_loop__(root, add_next)
         else:
-            loop = self.add_for_loop(root, add_next)
-        self.add_dc_motors_in_loop(loop)
+            loop = self.__add_for_loop__(root, add_next)
+        self.__add_dc_motors_in_loop__(loop)
         return loop
 
-    def add_while_loop(self, root, add_next):
+    def __add_while_loop__(self, root, add_next):
         if add_next:
             root = ET.SubElement(root, 'next')
         while_loop = copy.deepcopy(random.choice(self.while_loops))
         root.append(while_loop)
         return while_loop[2][0]
 
-    def add_for_loop(self, root, add_next):
+    def __add_for_loop__(self, root, add_next):
         if add_next:
             root = ET.SubElement(root, 'next')
         for_loop = copy.deepcopy(random.choice(self.for_loops))
@@ -121,7 +122,7 @@ class ScenarioDataGenerator:
         if self.debug: print(for_loop.attrib.get("type"))
         return for_loop
 
-    def add_dc_motors_in_loop(self, loop):          
+    def __add_dc_motors_in_loop__(self, loop):          
         dc_motors = copy.deepcopy(random.choice(self.dc_motors))
         if loop.attrib.get("type") == 'controls_for':
             loop[4].append(dc_motors)
@@ -161,5 +162,9 @@ print(ast[0])
 
 print("\n\n")
 
-igraph = analyser.convert_ast_trees_to_igraph_ast_trees(ast)
-print(igraph[0])
+igraphs = analyser.convert_ast_trees_to_igraph_ast_trees_keep_label(ast)
+igraph_obj = igraphs[0]
+
+layout = igraph_obj.layout("kk")
+igraph_obj.vs["label"] = igraph_obj.vs["name"]
+igraph.plot(igraph_obj, layout=layout, bbox=(1600,900), margin=40, vertex_label_dist=-3)
